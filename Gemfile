c14e31db9cc1894f6c6b9cd716c50782fdf3ea80
Robert Tassy <Rtass001@fiu.edu>
2016-02-02 11:23:38 -0500

    new server project

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# assume magical stuff is being required up here
+
+source "https://rubygems.org"
+ruby "2.3.0"
+
+gem "sinatra"
---
+++ server.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+# defines a domain-specific language (DSL).
+# dedicated solely to making new websites.
+
+get '/' do
+  "hello world!"
+end
+
+get '/sinatra' do
+  "hello world"
+end
+
+# heroku is a platform as a service (accessing something that runs on the internet)
+# 
